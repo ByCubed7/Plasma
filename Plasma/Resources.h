a48c62de032bc92330937c8ded204821102500d6
ByCubed7 <ByCubed7@gmail.com>
2021-10-20 22:24:34 +0100

    Added the Font class Updated the Resources

--- Plasma/Resources.h
+++ Plasma/Resources.h
@@ -1,4 +1,4 @@
-#include <map>
+﻿#include <map>
 #include <string>
 
 #include "Library/glad.h"
@@ -11,11 +11,11 @@
 class Resources {
 public:
 	
+	// █▀ █ █ ▄▀█ █▀▄ █▀▀ █▀█
+	// ▄█ █▀█ █▀█ █▄▀ ██▄ █▀▄
+
 	// All the shaders we have cached
 	static std::map<std::string, Shader> Shaders;
-
-	// All the textures we have cached
-	static std::map<std::string, Texture2D> Textures;
 
 	// Loads a shader from file loading vertex, fragment shader's source code. 
 	// If gShaderFile is not nullptr, it also loads a geometry shader
@@ -24,12 +24,36 @@ public:
 	// Retrieves a cached shader
 	static Shader& GetShader(std::string name);
 	
+
+	//▀█▀ █▀▀ ▀▄▀ ▀█▀ █ █ █▀█ █▀▀
+	// █  ██▄ █ █  █  █▄█ █▀▄ ██▄
+
+	// All the textures we have cached
+	static std::map<std::string, Texture2D> Textures;
+
 	// Loads a texture
 	static Texture2D LoadTexture(const char* file, bool alpha, std::string name);
 	
 	// Retrieves a cached texture
 	static Texture2D& GetTexture(std::string name);
 	
+
+	// █▀▀ █▀█ █▄ █ ▀█▀
+	// █▀  █▄█ █ ▀█  █ 
+
+	// All the fonts we have cached
+	static std::map<std::string, Texture2D> Textures;
+	
+	// Loads a font
+	static void LoadFont(const char* file, std::string name);
+	
+	// Retrieves a cached font
+	static void GetFont(std::string name);
+
+	// - - - - - - - - - - - - - - - - - - - - - - - -
+	// - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 	// Properly de-allocates all loaded resources
 	// The deconstructor, basically
 	static void Clear();
@@ -40,7 +64,17 @@ private:
 
 	// Loads a shader from a file
 	static Shader LoadShaderFromFile(const char* vShaderFile, const char* fShaderFile, const char* gShaderFile = nullptr);
-	
+
 	// Loads a texture from a file
 	static Texture2D LoadTextureFromFile(const char* file, bool alpha);
+
+	// Loads a font from a file
+	static void LoadTextureFromFile(const char* file);
+};
+
+struct Character {
+	unsigned int TextureID;  // ID handle of the glyph texture
+	glm::ivec2   Size;       // Size of glyph
+	glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+	unsigned int Advance;    // Offset to advance to next glyph
 };
--- Plasma/main.cpp
+++ Plasma/main.cpp
@@ -11,12 +11,6 @@
 #include <ft2build.h>]
 #include FT_FREETYPE_H
 
-struct Character {
-	unsigned int TextureID;  // ID handle of the glyph texture
-	glm::ivec2   Size;       // Size of glyph
-	glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
-	unsigned int Advance;    // Offset to advance to next glyph
-};
 
 //void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 //void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
