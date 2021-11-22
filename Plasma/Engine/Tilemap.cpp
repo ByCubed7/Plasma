57bb63b2391b157a37f568a0ceb3f70b29c69776
BuildTools <38486990+ByCubed7@users.noreply.github.com>
2021-11-22 11:22:54 +0000

    Created a Tilemap and Tilemap Renderer class Ready for implementation

---
+++ Plasma/Engine/Tilemap.cpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "SpriteRenderer.h"
+#include "Shader.h"
+#include "Font.h"
+#include "Resources.h"
+
+SpriteRenderer::SpriteRenderer(Shader& shader)
+{
+    this->shader = shader;
+    this->initRenderData();
+}
+
+SpriteRenderer::~SpriteRenderer()
+{
+    glDeleteVertexArrays(1, &this->quadVAO);
+}
+
+void SpriteRenderer::DrawSprite(Texture2D& texture, glm::vec2 position, glm::vec2 size, float rotate, int frame, glm::vec3 color)
+{
+    // Prepare transformations
+    this->shader.Use();
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(position, 0.0f)); // translate
+
+    model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f)); // move origin of rotation to center of quad
+    model = glm::rotate(model, glm::radians(rotate), glm::vec3(0.0f, 0.0f, 1.0f)); // rotate
+    model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f)); // move origin back
+
+    model = glm::scale(model, glm::vec3(size, 1.0f)); // scale
+
+    this->shader.SetMatrix4("model", model);
+
+    // render textured quad
+    this->shader.SetVector3f("spriteColor", color);
+
+    // Set shadow render dims
+    this->shader.SetVector2f("spriteSize", { texture.Height, texture.Height });
+
+    // Texture Frame
+    this->shader.SetInteger("index", frame);
+
+    glActiveTexture(GL_TEXTURE0);
+    texture.Bind();
+
+
+    glBindVertexArray(this->quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
+
+void SpriteRenderer::initRenderData()
+{
+    // configure VAO/VBO
+    unsigned int VBO;
+    float vertices[] = {
+        // pos      // tex
+        0.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 0.0f,
+
+        0.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &this->quadVAO);
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(this->quadVAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+
+    // Set filtering options
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+}
